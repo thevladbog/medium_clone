8b43dd7782a9dab880f2a89fef64ea7014c8c10d
Vladislav Bogatyrev <vladislav.bogatyrev@gmail.com>
2022-06-10 21:24:39 +0300

    fix: slug generation

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from 'src/user/guards/auth.guard';
 import { UserEntity } from 'src/user/user.entity';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/createArticle.dto';
+import { ArticleResponceInterface } from './types/articleResponse.interface';
 
 @Controller('articles')
 export class ArticleController {
@@ -14,7 +15,11 @@ export class ArticleController {
   async create(
     @User() curreentUser: UserEntity,
     @Body('article') createArticleDto: CreateArticleDto,
-  ): Promise<any> {
-    return this.articleService.createArticle(curreentUser, createArticleDto);
+  ): Promise<ArticleResponceInterface> {
+    const article = await this.articleService.createArticle(
+      curreentUser,
+      createArticleDto,
+    );
+    return this.articleService.buildArticleResponse(article);
   }
 }
