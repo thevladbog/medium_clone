fa646cc31e6de09e12aae9662632a1ef6d4e56fc
Vladislav Bogatyrev <vladislav.bogatyrev@gmail.com>
2022-06-12 23:48:06 +0300

    new: profile module

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from 'src/user/decorators/user.decorator';
+import { ProfileService } from './profile.service';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
+
+@Controller('profiles')
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+  @Get(':username')
+  async getProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.getProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+}
